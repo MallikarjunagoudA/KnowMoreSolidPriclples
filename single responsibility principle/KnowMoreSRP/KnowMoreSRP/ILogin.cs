c79757eed57f55e286b09acfa8cf9a86c2768259
MallikarjungoudA <aamallikarjuna18@gmail.com>
2022-05-03 20:55:55 +0530

    SRP

---
+++ single responsibility principle/KnowMoreSRP/KnowMoreSRP/ILogin.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KnowMoreSRP
+{
+    interface ILogin 
+    {
+        public void signup(string username, string password, string email, string phone);
+        public void Login(string username, string password);
+    }
+
+    interface ErrorHandle
+    {
+        public void ErrorHandle(string err);
+    }
+
+    interface SendMail
+    {
+        public void SendMail(string subject, string body);
+    }
+
+
+}
---
+++ single responsibility principle/readme.txt
@@ -0,0 +1,5 @@
+Here the SRP console contails Login interface.
+
+where
+
+we separate the each interface with each functionality so that each class will be specified with each model.
