6f256d9bfc549a8188933972e1941daf0c387b69
MallikarjungoudA <aamallikarjuna18@gmail.com>
2022-05-05 09:11:04 +0530

    OCP

---
+++ Open Close principle/OpenClosePrinciple/OpenClosePrinciple/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace OpenClosePrinciple
+{
+
+    public abstract  class Employee
+    {
+        public void ageCalculate(DateTime DOB)
+        {
+            //calculate age
+        }
+
+        public void empFullName(string fn,string mn, string ln)
+        {
+            var fullName = fn + mn + ln;
+        }
+
+        public abstract string Bonus(string salary);
+    }
+
+    public class tempEmp : Employee
+    {
+        public override string Bonus(string salary)
+        {
+            return Convert.ToString(Convert.ToInt32(salary) * 10);
+        }
+    }
+
+    public class PermanenetEmp : Employee
+    {
+        public override string Bonus(string salary)
+        {
+            return Convert.ToString(Convert.ToInt32(salary) * 15);
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
